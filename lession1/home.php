fb46980342b0f18de1a094d33c0e91ab4cde9ac8
TaiND <taind@ai-t.vn>
2016-04-06 14:55:05 +0700

    Update

--- lession1/home.php
+++ lession1/home.php
@@ -2,30 +2,14 @@
 <head>
 </head>
 <body>
-
-<h3>Login</h3>
-<hr>
-
-
-<form action="login.php" method="post">
-    User Name:<br>
-    <input type="text" name="user_name"><br>
-    Last Name:<br>
-    <input type="password" name="pass_word"><br>
-    <input type="submit" value="Login">
-</form>
-
 <h3>News</h3>
 <hr>
 <ul>
+    <?php foreach ($dataNews as $id => $item) { ?>
     <li>
-        <?php
-
-        foreach ($dataNews as $id => $item) {
-            echo '<a href="detail.php?route=detail&id=' . $id . '"><h2>' . $item['key'] . '</h2></a>';
-            echo "<div>" . $item['content'] . "</div>";
-        }
-        ?>
+        <h3><a href="detail.php?id=<?php echo $id ?>"><?php echo $item['key'] ?></a></h3>
+        <p><?php echo $item['content'] ?></p>
+        <?php } ?>
     </li>
 </ul>
 
